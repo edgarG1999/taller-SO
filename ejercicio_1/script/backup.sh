8b725993eedf9c14339ef18442147080837fff5d
edgarG1999 <ef.edgar1999@gmail.com>
2025-04-10 19:13:13 -0400

    Quinto commmit

--- ejercicio_1/script/backup.sh
+++ ejercicio_1/script/backup.sh
@@ -5,6 +5,7 @@ ORIGEN="/home/edgar/Escritorio/taller_SO/ejercicio_1/script"
 DESTINO="/home/edgar/Escritorio/taller_SO/ejercicio_1/backups"
 NOMBRE_BACKUP="respaldo_$(date +%Y-%m-%d).tar.gz"
 REPO_DIR="/home/edgar/Escritorio/taller_SO"
+LOG_FILE="/home/edgar/Escritorio/taller_SO/ejercicio_1/backups/backup.log"
 
 echo "Iniciando proceso de respaldo y versionado..."
 
@@ -17,12 +18,14 @@ if [ ! -f "$DESTINO/$NOMBRE_BACKUP" ]; then
 echo "Error al crear el respaldo!"
 exit 1
 fi
+
 # 3. Copiar respaldo al repositorio
-cp "$DESTINO/$NOMBRE_BACKUP" "$REPO_DIR/backups/"
+cp "$DESTINO/$NOMBRE_BACKUP" "$REPO_DIR/"
 
 # 4. Actualizar repositorio Git
 cd "$REPO_DIR"
-git add backups/"$NOMBRE_BACKUP"
+git add "$NOMBRE_BACKUP"
 git commit -m "Respaldo automático: $(date +%Y-%m-%d)"
-git push origin main
+git push origin master
+
 echo "Proceso completado con éxito!"
